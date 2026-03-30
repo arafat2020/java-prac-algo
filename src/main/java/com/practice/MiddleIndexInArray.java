7aaa073d5be395cee95567b8de4814c9c7717af2
arafat2020 <arafatmannan2019@gmail.com>
2026-03-30 11:08:25 +0600

    ADD: MIddle index of an array

---
+++ src/main/java/com/practice/MiddleIndexInArray.java
@@ -0,0 +1,24 @@
+package com.practice;
+
+public class MiddleIndexInArray {
+    public int findMiddleIndex(int[] nums) {
+        int[] prefex_arr = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            prefex_arr[i] = prefex_arr[i - 1] + nums[i - 1];
+        }
+       +
+
+        for (int i = 0; i < nums.length; i++) {
+            if (prefex_arr[i] == prefex_arr[nums.length] - prefex_arr[i + 1]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        MiddleIndexInArray middleIndexInArray = new MiddleIndexInArray();
+        int[] nums = { 2, 3, -1, 8, 4 };
+        System.out.println(middleIndexInArray.findMiddleIndex(nums));
+    }
+}
