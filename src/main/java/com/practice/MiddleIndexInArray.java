fce4aae73c51339232dcfa1a4721a498e56e88cc
arafat2020 <arafatmannan2019@gmail.com>
2026-03-30 11:09:19 +0600

    Minor adjustement

--- src/main/java/com/practice/MiddleIndexInArray.java
+++ src/main/java/com/practice/MiddleIndexInArray.java
@@ -6,7 +6,6 @@ public class MiddleIndexInArray {
         for (int i = 1; i <= nums.length; i++) {
             prefex_arr[i] = prefex_arr[i - 1] + nums[i - 1];
         }
-       +
 
         for (int i = 0; i < nums.length; i++) {
             if (prefex_arr[i] == prefex_arr[nums.length] - prefex_arr[i + 1]) {
