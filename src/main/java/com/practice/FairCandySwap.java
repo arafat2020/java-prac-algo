388457896619e51c3125c1f6644c6af0d229580e
arafat2020 <arafatmannan2019@gmail.com>
2026-04-01 16:14:24 +0600

    ADD

---
+++ src/main/java/com/practice/FairCandySwap.java
@@ -0,0 +1,42 @@
+package com.practice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FairCandySwap {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int sumA = 0;
+        int sumB = 0;
+        Set<Integer> arr_set = new HashSet<>();
+
+        for (int i : aliceSizes) {
+            sumA += i;
+        }
+
+        for (int i : bobSizes) {
+            sumB += i;
+            arr_set.add(i);
+        }
+
+
+        for (int i : aliceSizes) {
+            int diff = Math.abs((sumA - sumB)) / 2 + i;
+            if (arr_set.contains(diff)) {
+                
+                return new int[]{i , diff};
+                
+            }
+        }
+
+        return new int[]{};
+
+    }
+
+    public static void main(String[] args) {
+        FairCandySwap fairCandySwap = new FairCandySwap();
+        int[] aliceSizes = {1, 2, 5};
+        int[] bobSizes = {2, 4};
+        int[] result = fairCandySwap.fairCandySwap(aliceSizes, bobSizes);
+        System.out.println("Alice should swap: " + result[0] + " with Bob's: " + result[1]);
+    }
+}
